3190c9523c7078b0e97d08d9e906eb7c91f3a408
a-labrou <87115067+a-labrou@users.noreply.github.com>
2021-07-29 15:46:36 +0300

    Update build-msi.bat

--- Installer/Windows/build-msi.bat
+++ Installer/Windows/build-msi.bat
@@ -1,8 +1,9 @@
 @echo off
 
 rmdir /s /q Duplicati 
-del /q Installer/Windows/Duplicati.msi    rem Could Not Find D:\a\duplicati-local\duplicati-local\Duplicati.msi
-del /q Installer/Windows/Duplicati-32bit.msi   rem δεν εχουν δημιουργηθει τα αρχεια αρα δε μπορει να τα διαγραψει
+
+del /q Duplicati.msi         rem Could Not Find D:\a\duplicati-local\duplicati-local\Duplicati.msi
+del /q Duplicati-32bit.msi   rem δεν εχουν δημιουργηθει τα αρχεια αρα δε μπορει να τα διαγραψει, αν βαλω path λεει invalid switch windows
 
 IF NOT EXIST "%1" (
 	echo File not found, please supply a zip file with the build as the first argument
