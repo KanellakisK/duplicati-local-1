43844c0fb949b5cf55c088afd15f13f982db025f
a-labrou <87115067+a-labrou@users.noreply.github.com>
2021-07-29 14:23:39 +0300

    Update build-msi.bat

--- Installer/Windows/build-msi.bat
+++ Installer/Windows/build-msi.bat
@@ -102,27 +102,35 @@ IF EXIST "..\..\..\oem-update-installid.txt" (
 	echo Installing OEM override file
 	xcopy ..\..\..\oem-update-installid.txt Duplicati /e /s /y /i
 )
+
 echo ---****---****ΒΓΗΚΕ ΑΠΟ ΤΗΝ ΤΕΛΕΥΤΑΙΑ IF KAI ΘΑ ΚΑΝΕΙ rndir obj,bin---****---****
+
 rmdir /s /q obj      rem The system cannot find the file specified
 rmdir /s /q bin      rem The system cannot find the file specified
 
 echo ---****---****ΣΥΝΕΧΙΖΕΙ ,ΔΕ ΜΠΟΡΕΙ ΝΑ ΒΡΕΙ ΤΟ  UpgradeData.wxi.orig, αν βαλω path μονο στο UpgradeData.wxi ειναι invalid syntax  ---****---****
-copy UpgradeData.wxi UpgradeData.wxi.orig     rem  The system cannot find the file specified
+
+copy "Installer\Windows\UpgradeData.wxi" UpgradeData.wxi.orig     rem  The system cannot find the file specified
 
 echo ---****---***MΕ ΠΛΗΡΗ PATHS EΓΙΝΕ UPDATE TO UpgradeData.wxi---****---****
+
 "Installer/Windows/UpdateVersion.exe" "Installer/Windows/Duplicati/Duplicati.GUI.TrayIcon.exe" "Installer/Windows/UpgradeData.wxi" 
 
 echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΘΑ ΧΤΥΠΗΣΕΙ ΤΟ msbuild x64, ΔΕΝ ΕΧΕΙ ΟΡΙΣΜΑ--****---****
+
 msbuild /property:Configuration=Release /property:Platform=x64    rem error MSB1011: Specify which project or solution file to use because this folder contains more than one project or solution file.
 
 echo ---****---****ΣΥΝΕΧΙΖΕΙ, ΔΕ ΒΡΙΣΚΕΙ ΤΟ Duplicati msi, ΔΕ ΜΠΟΡΕΙ ΝΑ ΚΑΝΕΙ move, ΙΣΩΣ ΕΠΕΙΔΗ ΔΕΝ ΕΧΕΙ ΦΤΙΑΧΤΕΙ Ο ΦΑΚΕΛΟΣ bin---****---****
+
 move bin\x64\Release\Duplicati.msi Duplicati.msi    rem The system cannot find the path specified.
 
 echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΙΔΙΑ ERROR ΜΕ ΠΡΙΝ ΓΙΑ msbuild x86, Duplicati.msi ---****---****
+
 msbuild /property:Configuration=Release /property:Platform=x86      rem MSBUILD : error MSB1008: Only one project can be specified
 move bin\x86\Release\Duplicati.msi Duplicati-32bit.msi              rem The system cannot find the path specified.
 
 echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΘΑ ΧΤΥΠΗΣΕΙ ΔΥΟ ΦΟΡΕΣ ΤΟ UpgradeData.wxi.orig, ΔΕ ΤΟ ΒΡΙΣΚΕΙ ΚΑΙ ΔΕ ΜΠΟΡΕΙ ΝΑ ΚΑΝΕΙ copy,del---****---****
+
 copy UpgradeData.wxi.orig "Installer/Windows/UpgradeData.wxi"  rem Could Not Find D:\a\duplicati-local\duplicati-local\UpgradeData.wxi.orig
 del UpgradeData.wxi.orig                                       rem Could Not Find D:\a\duplicati-local\duplicati-local\UpgradeData.wxi.orig
 
