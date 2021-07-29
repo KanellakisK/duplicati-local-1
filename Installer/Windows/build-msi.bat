f9231fb62cc5ac91b9e3a41adbebbdd2451547ea
a-labrou <87115067+a-labrou@users.noreply.github.com>
2021-07-29 10:27:56 +0300

    Update build-msi.bat

--- Installer/Windows/build-msi.bat
+++ Installer/Windows/build-msi.bat
@@ -107,7 +107,7 @@ rmdir /s /q obj
 rmdir /s /q bin
 
 copy UpgradeData.wxi UpgradeData.wxi.orig                              rem  The system cannot find the file specified
-UpdateVersion.exe Duplicati\Duplicati.GUI.TrayIcon.exe UpgradeData.wxi rem 'UpdateVersion.exe' is not recognized as an internal or external command,
+./Installer/Windows/Duplicati/UpdateVersion.exe Duplicati\Duplicati.GUI.TrayIcon.exe UpgradeData.wxi rem 'UpdateVersion.exe' is not recognized as an internal or external command,
 
 msbuild /property:Configuration=Release /property:Platform=x64
 move bin\x64\Release\Duplicati.msi Duplicati.msi
