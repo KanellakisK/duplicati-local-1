fb883a7fea8eccc9c36993e9afef01fdb3d9812e
a-labrou <87115067+a-labrou@users.noreply.github.com>
2021-08-02 10:14:12 +0300

    Update build-msi.bat

--- Installer/Windows/build-msi.bat
+++ Installer/Windows/build-msi.bat
@@ -1,9 +1,8 @@
 @echo off
 
-rmdir /s /q Duplicati 
-
-del /q Duplicati.msi         rem Could Not Find D:\a\duplicati-local\duplicati-local\Duplicati.msi
-del /q Duplicati-32bit.msi   rem δεν εχουν δημιουργηθει τα αρχεια αρα δε μπορει να τα διαγραψει, αν βαλω path λεει invalid switch windows
+rmdir /s /q Duplicati
+del /q Duplicati.msi
+del /q Duplicati-32bit.msi
 
 IF NOT EXIST "%1" (
 	echo File not found, please supply a zip file with the build as the first argument
@@ -12,13 +11,13 @@ IF NOT EXIST "%1" (
 
 call "%VS140COMNTOOLS%vsvars32.bat"
 
- 7z x -oDuplicati %1
+7z x -oDuplicati %1
 
 IF EXIST "..\oem" (
 	echo Installing OEM files
 	xcopy ..\oem\* Duplicati\webroot\oem /e /s /y /i
 )
-echo ---****---****ΜΠΑΙΝΕΙ ΣΤΙΣ IF---****---****
+
 IF EXIST "..\..\oem" (
 	echo Installing OEM files
 	xcopy ..\..\oem\* Duplicati\webroot\ /e /s /y /i
@@ -104,45 +103,20 @@ IF EXIST "..\..\..\oem-update-installid.txt" (
 	xcopy ..\..\..\oem-update-installid.txt Duplicati /e /s /y /i
 )
 
-echo ---****---****ΒΓΗΚΕ ΑΠΟ ΤΗΝ ΤΕΛΕΥΤΑΙΑ IF, ΔΕ ΒΡΙΣΚΕΙ ΤΑ obj,bin ΓΙΑ ΝΑ ΚΑΝΕΙ rmdir, ουτε με path****---****
+rmdir /s /q obj
+rmdir /s /q bin
 
-rmdir /s /q obj      rem The system cannot find the file specified
-rmdir /s /q bin      rem The system cannot find the file specified
+copy UpgradeData.wxi UpgradeData.wxi.orig
+UpdateVersion.exe Duplicati\Duplicati.GUI.TrayIcon.exe UpgradeData.wxi
 
+msbuild /property:Configuration=Release /property:Platform=x64
+move bin\x64\Release\Duplicati.msi Duplicati.msi
 
-echo ---****---****ΣΥΝΕΧΙΖΕΙ ,ΔΕ ΜΠΟΡΕΙ ΝΑ ΒΡΕΙ ΤΟ  UpgradeData.wxi.orig, αν βαλω path μονο στο UpgradeData.wxi ειναι invalid syntax  ---****---****
+msbuild /property:Configuration=Release /property:Platform=x86
+move bin\x86\Release\Duplicati.msi Duplicati-32bit.msi
 
-copy UpgradeData.wxi UpgradeData.wxi.orig     rem  The system cannot find the file specified
-
-
-echo ---****---***MΕ ΠΛΗΡΗ PATHS EΓΙΝΕ UPDATE TO UpgradeData.wxi---****---****
-
-"Installer/Windows/UpdateVersion.exe" "Installer/Windows/Duplicati/Duplicati.GUI.TrayIcon.exe" "Installer/Windows/UpgradeData.wxi" 
-
-
-echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΘΑ ΧΤΥΠΗΣΕΙ ΤΟ msbuild x64, ΔΕΝ ΕΧΕΙ ΟΡΙΣΜΑ--****---****
-
-msbuild /property:Configuration=Release /property:Platform=x64    rem error MSB1011: Specify which project or solution file to use because this folder contains more than one project or solution file.
-
-
-
-echo ---****---****ΣΥΝΕΧΙΖΕΙ, ΔΕ ΒΡΙΣΚΕΙ ΤΟ Duplicati msi, ΔΕ ΜΠΟΡΕΙ ΝΑ ΚΑΝΕΙ move, ΙΣΩΣ ΕΠΕΙΔΗ ΔΕΝ ΕΧΕΙ ΦΤΙΑΧΤΕΙ Ο ΦΑΚΕΛΟΣ bin---****---****
-
-move bin\x64\Release\Duplicati.msi Duplicati.msi    rem The system cannot find the path specified.
-
-
-
-echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΙΔΙΑ ERROR ΜΕ ΠΡΙΝ ΓΙΑ msbuild x86, Duplicati.msi ---****---****
-
-msbuild /property:Configuration=Release /property:Platform=x86      rem MSBUILD : error MSB1008: Only one project can be specified
-move bin\x86\Release\Duplicati.msi Duplicati-32bit.msi              rem The system cannot find the path specified.
-
-
-
-echo ---****---****ΣΥΝΕΧΙΖΕΙ,ΘΑ ΧΤΥΠΗΣΕΙ ΔΥΟ ΦΟΡΕΣ ΤΟ UpgradeData.wxi.orig, ΔΕ ΤΟ ΒΡΙΣΚΕΙ ΚΑΙ ΔΕ ΜΠΟΡΕΙ ΝΑ ΚΑΝΕΙ copy,del---****---****
-
-copy UpgradeData.wxi.orig "Installer/Windows/UpgradeData.wxi"  rem Could Not Find D:\a\duplicati-local\duplicati-local\UpgradeData.wxi.orig
-del UpgradeData.wxi.orig                                       rem Could Not Find D:\a\duplicati-local\duplicati-local\UpgradeData.wxi.orig
+copy UpgradeData.wxi.orig UpgradeData.wxi
+del UpgradeData.wxi.orig
 
 rmdir /s /q Duplicati
 
